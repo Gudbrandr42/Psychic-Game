59d73f946bd6af8fbba20b727e5fda5dce62689d
Mark B <mark.bullard42@gmail.com>
2019-05-28 22:11:18 -0400

    got wins/losses/and reset showing and working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,36 @@
+/*window.onload = function()*/
 var wins = 0;
 var losses = 0;
-var remaining = 0;
+var remaining = 10;
 var resetArray = [];
-var pickLetter = pickLetter;
+var pickLetter = "";
 var letters = "abcdefghijklmnopqrstuvwxyz";
 
-pickletter = letters[Math.floor(Math.random() * letters.length)];
-console.log(pickletter);
-
-function guess() {
-  pickLetter = letters[Math.floor(Math.random() * letters.length)];
-  console.log(pickLetter);
-}
+pickLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(pickLetter);
 
 document.onkeyup = function(event) {
   var userChoice = event.key;
+  console.log(userChoice);
   if (userChoice === pickLetter) {
-    win++;
-    guessesLeft = 10;
-    resetArray = [];
-  }
-
-  guess();
-  if (userChoice !== pickLetter) {
+    wins++;
+    console.log(wins);
+  } else {
     remaining--;
   }
 
-  if (remaining == 0) {
-    loss++;
-    resetArray = [];
-    guessesLeft = 10;
+  if (remaining === 0) {
+    losses++;
+    console.log(losses);
+    remaining = 10;
   }
 
-  if (resetArray.indexOf(userChoice) >= 0) {
-  } else {
-    resetArray.push(userChoice);
-    document.getElementById("userChoice").innerHTML = resetArray;
-    console.log(resetArray);
-  }
+  // if (resetArray.indexOf(userChoice) >= 0) {
+  // } else {
+  //   resetArray.push(userChoice);
+  //   document.getElementById("userChoice").innerHTML = resetArray;
+  //   console.log(resetArray);
+  // }
 
   document.getElementById("win").innerHTML = wins;
   document.getElementById("loss").innerHTML = losses;
